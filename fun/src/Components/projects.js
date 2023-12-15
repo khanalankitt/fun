6b73d5ba1b949cc72d8899e8ce98d39361b3d0aa
Ankit Khanal <akhanal749@gmail.com>
2023-12-15 21:17:20 +0545

    uploaded

--- fun/src/Components/projects.js
+++ fun/src/Components/projects.js
@@ -1,7 +1,19 @@
 import "../App.css";
 import "../Projects.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHtml5, faCss3Alt, faJs ,faReact} from "@fortawesome/free-brands-svg-icons";
 import { Me } from "./home";
+function printStack(skillStack) {
+    const iconMap = {
+      html: faHtml5,
+      css: faCss3Alt,
+      js: faJs,
+      react:faReact
+    };
+    return skillStack.map((skill, index) => (
+      <FontAwesomeIcon key={index} icon={iconMap[skill]} size="2x" style={{marginRight:"10px"}} />
+    ));
+}
 function ProjectItem(props){
     return (
         <>
@@ -17,7 +29,8 @@ function ProjectItem(props){
                         {props.summary}
                     </p>
                     <p style={{color:"#354765"}}><b>Tech Stack</b></p>
-                    <FontAwesomeIcon icon="fa-brands fa-html5" size="2xl" />
+                    {printStack(props.skillStack)}
+
                 </div>
                 <a href={props.link} target="_blank" rel="noreferrer">
                     <button>Live Demo ↗</button>
@@ -32,28 +45,28 @@ let projects = {
         projectName:"TodoList App",
         summary:"A simple app to create and keep track of things to do.",
         link:"https://khanalankitt.github.io/todoapp",
-        skillStack:["html","css","js","react"]
+        skillStack:["html","css","js"],
     },
     project2:{
         imageSource:"game.png",
         projectName:"Game",
         summary:"A simple rock paper scissors game to play when bored.",
         link:"https://khanalankitt.github.io/game",
-        skillStack:["html","css","js"]
+        skillStack:["html","css","js"],
     },
     project3:{
         imageSource:"smokefree.png",
         projectName:"Smokefree Teen",
         summary:"A website to educate teenagers about effects of smoking.",
         link:"https://khanalankitt.github.io/Smokefree",
-        skillStack:["html","css","js"]
+        skillStack:["html","css","js"],
     },
     project4:{
         imageSource:"chef.png",
         projectName:"Chef Website",
         summary:"A website showcasing the skills of a chef.",
         link:"https://khanalankitt.github.io/Chef",
-        skillStack:["html","css","js"]
+        skillStack:["html","css"],
 
     },
     project5:{
@@ -61,7 +74,7 @@ let projects = {
         projectName:"My first Portfolio",
         summary:"The first portfolio that I aired for other people to see.",
         link:"https://khanalankitt.github.io/Portfolio",
-        skillStack:["html","css","js"]
+        skillStack:["html","css"],
 
     },
     project6:{
@@ -69,7 +82,7 @@ let projects = {
         projectName:"My first React Portfolio",
         summary:"The first portfolio that I created using React JS.",
         link:"https://ankitkhanal.netlify.app",
-        skillStack:["html","css","js","react"]
+        skillStack:["html","css","js","react"],
     }
 }
 function Projects(){
@@ -82,43 +95,9 @@ function Projects(){
             you."
         />
         <div className="projects-container">
-            <ProjectItem
-                imageSource={projects.project1.imageSource}
-                projectName={projects.project1.projectName}
-                summary={projects.project1.summary}
-                link={projects.project1.link}
-            />
-            <ProjectItem
-                imageSource={projects.project2.imageSource}
-                projectName={projects.project2.projectName}
-                summary={projects.project2.summary}
-                link={projects.project2.link}
-            />
-            <ProjectItem
-                imageSource={projects.project3.imageSource}
-                projectName={projects.project3.projectName}
-                summary={projects.project3.summary}
-                link={projects.project3.link}
-            />
-            <ProjectItem
-                imageSource={projects.project4.imageSource}
-                projectName={projects.project4.projectName}
-                summary={projects.project4.summary}
-                link={projects.project4.link}
-            />
-            <ProjectItem
-                imageSource={projects.project5.imageSource}
-                projectName={projects.project5.projectName}
-                summary={projects.project5.summary}
-                link={projects.project5.link}
-            />
-            <ProjectItem
-                imageSource={projects.project6.imageSource}
-                projectName={projects.project6.projectName}
-                summary={projects.project6.summary}
-                link={projects.project6.link}
-            />
-            
+            {Object.values(projects).map((project, index) => (
+              <ProjectItem key={index} {...project} />
+            ))}   
         </div>
         </>
     );
